d0e14154e6f5470e054d77233a309a06caa10075
Amirhossein zanganeh <amirzanganeh1999@gmail.com>
2021-10-06 20:03:06 +0330

    .

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,8 @@
-import React , {useState} from 'react'
-
+import React , {useState , useEffect} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {validate} from './validate' 
+import {notify} from './toastify';
 function Signup() {
 
     const [data, setData] = useState({
@@ -9,32 +12,77 @@ function Signup() {
         confirmpassword:'',
         isAccept:false
     })
+    const [errors , setErrors] = useState({});
+    const [touched, setTouched]=useState({})
+
+        useEffect(() =>{
+        
+            setErrors(validate(data))
+            console.log(errors)
+        },[data,touched])
+
+
     const changehandler = event => {
-        setData({...data, [event.target.name]:event.target.value})
+        if(event.target.name === "isAccept"){
+            setData({...data,[event.target.name]:event.target.checked})
+        }else{
+            setData({...data, [event.target.name]:event.target.value})
+        }
     }
+    const focushandler = event =>{
+    setTouched({...touched, [event.target.name]:true})
+    }
+    const submithandler = event => {
+        event.preventDefault();
+        if(!Object.keys(errors).length){
+            notify('successfull' , 'success')
+        }else{
+            notify('error')
+            setTouched({
+                 name:true,
+            email:true,
+            password:true,
+            confirmpassword:true,
+            isAccept:false
+            })
+        }
+    }
+    
+
     return (
         <div>
+            <form>
             <h1>Signup</h1>
             <div>
                 <label>Name</label>
-                <input type="text" name='name' value={data.name} onChange={changehandler}/>
+                <input type="text" name='name' value={data.name} onChange={changehandler} onFocus={focushandler}/>
+                {errors.name && touched.name && <span>{errors.name}</span> }
             </div>
             <div>
                 <label>Email</label>
-                <input type="email" name='email' value={data.email} onChange={changehandler}/>
+                <input type="email" name='email' value={data.email} onChange={changehandler} onFocus={focushandler}/>
+                {errors.email && touched.email && <span>{errors.email}</span> }
             </div>
             <div>
                 <label>Password</label>
-                <input  type="password" name='password' value={data.password} onChange={changehandler}/>
+                <input  type="password" name='password' value={data.password} onChange={changehandler} onFocus={focushandler}/>
+                {errors.password && touched.password && <span>{errors.password}</span> }
             </div>
             <div>
                 <label>Password</label>
-                <input type="password" name='confirmpassword' value={data.confirmpassword} onChange={changehandler}/>
+                <input type="password" name='confirmpassword' value={data.confirmpassword} onChange={changehandler} onFocus={focushandler}/>
+                {errors.confirmpassword && touched.confirmpassword && <span>{errors.confirmpassword}</span> }
             </div>
             <div>
                 <label>I accept privacy and policy</label>
-                <input type="checkbox" name='isAccept' value={data.isAccept} onChange={changehandler}/>
+                <input type="checkbox" name='isAccept' value={data.isAccept} onChange={changehandler} onFocus={focushandler}/>
+                {errors.isAccept && touched.isAccept && <span>{errors.isAccept}</span> }
+                <div>
+                    <button onClick={submithandler}>Submit</button>
+                </div>
             </div>
+            </form>
+            <ToastContainer />
         </div>
     )
 }
---
+++ src/Components/validate.js
@@ -0,0 +1,38 @@
+export const validate = (data) => {
+  const errors = {};
+
+  if (!data.name.trim()) {
+    errors.name = "Username Requierd";
+  } else {
+    delete errors.name;
+  }
+
+  if (!data.email) {
+    errors.email = "Email Requierd";
+  } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+    errors.email = "Invalid Email ";
+  } else {
+    delete errors.email;
+  }
+
+  if (!data.password) {
+    errors.password = "Password Requierd";
+  } else if (data.password.length < 6) {
+    errors.password = "Password most be more than 6 chracters";
+  } else {
+    delete errors.password;
+  }
+  if (!data.confirmpassword) {
+    errors.confirmpassword = "Confirmpassword Requierd";
+  } else if (data.confirmpassword !== data.password) {
+    errors.confirmpassword = "Password not match";
+  } else {
+    delete errors.confirmpassword;
+  }
+  if (data.isAccept) {
+    delete errors.isAccept;
+  } else {
+    errors.isAccept = "check Required";
+  }
+  return errors;
+};
